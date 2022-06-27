910730a7d9ca97464d57a98cda205eb2c8923260
masudarina <104750749+masudarina@users.noreply.github.com>
2022-06-27 12:01:27 +0900

    Update trip.py

--- trip.py
+++ trip.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 st.title("Welcome to our page")
 st.write("Find a plce you wanna visit this summer!")
-image = Image.open('C:\Users\Systena\Pictures\ダウンロード.jfif')
+image = Image.open('C:\Users\Systena\Pictures\ダウンロード.')
 
 st.image(image, caption='サンプル',use_column_width=True)
 
