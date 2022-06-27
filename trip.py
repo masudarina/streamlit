091f14404a7644b41fac7a634b54e553396c1caa
masudarina <104750749+masudarina@users.noreply.github.com>
2022-06-27 13:49:08 +0900

    Update trip.py

--- trip.py
+++ trip.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 st.title("Welcome to our page")
 st.write("Find a plce you wanna visit this summer!")
-image = Image.open('C:\Users\Systena\Pictures\malta.png')
+image = Image.open('malta.png')
 
 st.image(image, caption='サンプル',use_column_width=True)
 
