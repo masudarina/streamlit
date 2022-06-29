1cb40fa041c6535064b13e9b42fe356063560386
masudarina <104750749+masudarina@users.noreply.github.com>
2022-06-29 11:00:24 +0900

    Update trip.py

--- trip.py
+++ trip.py
@@ -1,11 +1,9 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Welcome to our page")
+st.title("Welcome to our page✨")
 st.write("Find a plce you wanna visit this summer!")
-image = Image.open('malta.png')
+image = Image.open("C:\\Users\\Systena\\Pictures\\malta.png")
 
-st.image(image, caption='サンプル',use_column_width=True)
+st.image(image, caption='Malta Island',use_column_width=True)
 
-st.markdown("# Head1")
-st.markdown("## Head2")
