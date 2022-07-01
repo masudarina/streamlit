35d37ed9d10c5ce0da2701a53ee783fe1c5924a0
masudarina <104750749+masudarina@users.noreply.github.com>
2022-07-01 10:22:52 +0900

    Update titletest.py

--- titletest.py
+++ titletest.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("title")
-st.write("write")
-st.markdown("# Head1")
-st.markdown("## Head2")
+st.title('タイトル')
+st.header('ヘッダー')
+st.subheader('サブヘッダー')
+st.markdown('# マークダウンもかける(h1)')
+st.markdown('## マークダウンもかける(h2)')
+st.markdown('### マークダウンもかける(h3)')
+st.markdown('#### マークダウンもかける(h4)')
+st.markdown('##### マークダウンもかける(h5)')
+st.text('テキスト')
+st.info('情報')
+st.warning('警告')
+st.error('エラー')
